791b1ef2241be7a64726de8632608f1f7f733fc4
Sotiris Blad <sotblad@monetaryunit.org>
2021-04-21 00:04:48 +0300

    Update ask2.java

--- ask2/ask2.java
+++ ask2/ask2.java
@@ -7,10 +7,28 @@
 import javax.swing.*;
 
 public class ask2 {
-
+	private static Integer[][] blackPreset;
+	
+	public static Integer[][] setBlackPreset(int N, Integer[][] blackPreset) {
+		blackPreset = new Integer[N][N];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				blackPreset[i][j] = 0;
+			}
+		}
+		blackPreset[1][1] = 1;
+		blackPreset[2][1] = 1;
+		blackPreset[2][2] = 1;
+		return blackPreset;
+	}
+	
     public static void main(String[] args) {
-    	int N = 5;
-    	Board board = new Board(new JPanel(), N);
+    	
+    	int N = 7;
+    	
+    	blackPreset = setBlackPreset(N, blackPreset);
+    	
+    	Board board = new Board(new JPanel(), N, blackPreset);
 
     	JFrame f = new JFrame("Board");
         f.add(board.getGui());
